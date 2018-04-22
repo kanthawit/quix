d8405bbbb789ed19c712225ca1810d93e2546fb5
taczclub <henryroy40@gmail.com>
2018-04-23 00:11:32 +0530

    Termux localhost

--- quix.sh
+++ quix.sh
@@ -1,7 +1,10 @@
-apt-get -y update 
-apt-get -y upgrade 
-apt-get install -y php
-cd /data/data/com.termux/files/usr/bin
+apt update
+echo "y"
+apt upgrade
+echo "y"
+pkg install php
+echo "y"
 wget https://github.com/taczclub/files/blob/master/localhost
+cp $HOME/quix/localhost /data/data/com.termux/files/usr/bin
 echo "localhost installation is successful now Execute localhost by type localhost"
 echo "DONE"
