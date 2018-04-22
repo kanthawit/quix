2bf72ee7c26d4bc9aefb4394a0056b1b8a4583ab
taczclub <henryroy40@gmail.com>
2018-04-23 00:17:02 +0530

    Termux localhost

--- quix.sh
+++ quix.sh
@@ -5,6 +5,10 @@ echo "y"
 pkg install php
 echo "y"
 wget https://github.com/taczclub/files/blob/master/localhost
+cd quix
+chmod 777 localhost
 cp $HOME/quix/localhost /data/data/com.termux/files/usr/bin
+cd /data/data/com.termux/files/usr/bin
+chmod 777 localhost
 echo "localhost installation is successful now Execute localhost by type localhost"
 echo "DONE"
