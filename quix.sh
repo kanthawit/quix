e05605800e9ea6c7e267f0bba67791af93789fa6
taczclub <henryroy40@gmail.com>
2018-04-23 00:59:28 +0530

    Update quix.sh

--- quix.sh
+++ quix.sh
@@ -4,10 +4,11 @@ apt upgrade
 echo "y"
 pkg install php
 echo "y"
+cd $HOME
 git clone https://github.com/taczclub/localhost
-cd quix
+cd localhost
 chmod 777 localhost
-cp $HOME/quix/localhost /data/data/com.termux/files/usr/bin
+cp $HOME/localhost/localhost /data/data/com.termux/files/usr/bin
 cd /data/data/com.termux/files/usr/bin
 chmod 777 localhost
 echo "localhost installation is successful now Execute localhost by type localhost"
