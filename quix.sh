2c970c0b657266298c54184ab2bbbf72f82be10b
taczclub <henryroy40@gmail.com>
2018-04-23 18:02:19 +0530

    Update quix.sh

--- quix.sh
+++ quix.sh
@@ -6,7 +6,17 @@ cp $HOME/localhost/localhost /data/data/com.termux/files/usr/bin
 rm -rf localhost
 cd /data/data/com.termux/files/usr/bin
 chmod 777 localhost
-echo "localhost installation is successful now Execute localhost by type localhost"
+cd /sdcard
+mkdir htdocs
+cd /sdcard/htdocs
+git clone https://github.com/taczclub/helloworld
+mv /sdcard/htdocs/helloworld/index.php /sdcard/htdocs
+rm -rf helloworld
+echo "localhost installation is successful now Execute localhost by type localhost."
+echo "PUT YOUR LOCALHOST CONTENTS INTO INTERNAL SDCARD IN htdocs FOLDER.
 echo "DONE"
 cd
-rm -rf quix
+rm -rf quix 
+
+
+
